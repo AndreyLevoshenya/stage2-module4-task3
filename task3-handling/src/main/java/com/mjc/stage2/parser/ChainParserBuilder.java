7490deb003d2cf08decc7e56e7c8efdc822329f7
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-06-25 11:24:00 +0300

    Add files via upload

--- task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
@@ -16,13 +16,18 @@ public class ChainParserBuilder {
         return this;
     }
 
-    public AbstractTextParser build() throws HandlingException {
-        if(parsers.isEmpty()) {
-            throw new HandlingException();
-        }
-        AbstractTextParser chain = parsers.get(0);
-        for (int i = 1; i < parsers.size(); i++) {
-            chain.setNextParser(parsers.get(i));
+    public AbstractTextParser build() {
+        AbstractTextParser chain = null;
+        try {
+            if(parsers.isEmpty()) {
+                throw new HandlingException();
+            }
+            chain = parsers.get(0);
+            for (int i = 1; i < parsers.size(); i++) {
+                chain.setNextParser(parsers.get(i));
+            }
+        } catch (HandlingException e) {
+            e.printStackTrace();
         }
         return chain;
     }
